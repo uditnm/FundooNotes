9fdcf7ff546f90752cf6625edbf882cf2444ba27
uditnm <udit20001@gmail.com>
2023-03-20 11:44:16 +0530

    [Udit] Added Label Entity.

---
+++ RepositoryLayer/Entity/LabelEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class LabelEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LabelId { get; set; }
+        public string LabelName { get; set; }
+        [ForeignKey("NotesEntity")]
+        public long NoteId { get; set; }
+        public NotesEntity NotesEntity { get; set; }
+        [ForeignKey("UserEntity")]
+        public long UserId { get; set; }
+        public UserEntity UserEntity { get; set; }
+    }
+}
